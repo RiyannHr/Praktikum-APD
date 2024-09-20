a6263ec0b856fe5a900e0fc9b7218033480f133d
RiyannHr <riyan.hidayat.rahman1@gmail.com>
2024-09-20 15:18:59 +0800

    Add files via upload

---
+++ KelasB2_24/Pertemuan 3/2409106085_RiyanHidayatRahman_B2_24.py
@@ -0,0 +1,103 @@
+# if cuaca == "Cerah"
+
+# if cuaca == "cerah":
+#     print("Kamu pergi keluar rumah")
+
+
+
+
+
+
+
+
+
+
+# cuaca = input("Masukan cuaca hari ini: ")
+# if cuaca == "cerah":
+#     print ("kamu keluar rumah")
+
+# cuaca = input("Masukan cuaca hari ini: ")
+# if cuaca == "cerah":
+#     print("Kamu keluar rumah")
+# else:
+#     print("kamu keluar rumah")
+
+cuaca = input("masukan cuaca hari ini: ")
+if cuaca ==  "cerah":
+
+
+
+
+
+    
+    
+    
+    
+    
+    
+    
+   # if opsi == "1":
+   #     print("kondisi 1")
+   # elif opsi == "2":
+   #      print("kondisi 2")
+   # elif opsi == "3":
+   #      print("kondisi 3")
+   # else:
+   #     print("input tidak valid")
+   
+   
+   
+   # if umur <0:
+   #     print("input tidak valid")
+   # elif (umur <= 10) :
+   #     print("anak anak")
+   # elif (umur <= 18):
+   #     print("remaja")
+   # elif (umur <= 60):
+   
+   
+        
+    
+        
+         
+   
+   
+   
+   
+   
+   
+   umur = input("masukan umur: ")
+   if (umur <= 10) and 
+   
+  
+
+  
+  
+  
+  
+  #penggunaan and, or
+  a = 10
+  b = 20
+  if a < b and a % 2 == 0:
+      print("a lebih kecil dari b dan a adalah bilangan genap")  
+  elif a > b or a % 2 == 0:
+      print("a lebih besar dari b atau a adalah bilangan genap") 
+  else:
+      print(" a lebih kecil dari b dan a adalah bilangan ganjil")
+      
+      
+      
+      
+      
+  opsi = input("pilih menu: ")
+  match opsi:
+      case "1";
+        print("kondisi 1")
+      case "2":
+          print("kondisi 2")
+      case "3":
+           print ("kondisi 3")
+      case _;
+          print("input tidak valid")
+          
+      
