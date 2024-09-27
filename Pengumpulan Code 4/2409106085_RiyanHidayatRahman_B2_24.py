5e7150849a15d2964901e941069939c3f5b40afa
RiyannHr <riyan.hidayat.rahman1@gmail.com>
2024-09-27 15:19:11 +0800

    Add files via upload

---
+++ Pengumpulan Code 4/2409106085_RiyanHidayatRahman_B2_24.py
@@ -0,0 +1,62 @@
+# ulang = 10
+# for i in range(ulang):
+    # print ("perulangan ke-"+str(i+ " kali")
+    # print(f"perulangan ke --{i} kali"
+     
+#     for i in range(1, 11, 3)
+     # print(f"perulangan ke--{i} kali"
+
+# simpan = [12, "udin petot", 14.5, True, 'A']
+# for i in simpan:
+#      print(i)
+
+# print(sampai[2])
+
+# for i in range(1, 4):
+#   for j in range(1, 4):
+#        print(f"{i} x {j} = {i + j}")
+#    print()
+#    for i in range
+
+# Buat perulangan Anak ayam turun 10
+# for i range(10, -1, -1 ):
+#      print(f"Anak ayam turun {i}")
+      
+# while ulang == "ya":
+#   print(f"Perulangan ke {hitung}")
+#   ulang = input("Apakah anda masih ingin mengulang?")
+#   hitung +=1
+#   print("Perulangan selesai")
+
+#   hitung +=1
+# print ("perulangan selesai")
+
+# for i range(10):
+#    if i % 2 == 0:
+#        continue
+#    if i == 5:    
+#        continue #lompat ke literasi selanjutnya
+#    print(i)
+
+#buat program login, jika salahnya sebanyak 3x, maka program berhenti
+
+usn = "admin"
+pw = "admin123"
+salah = 0
+
+while salah < 3:
+    username = input("Masukan username: ")
+    password = input("Masukkan password ")
+    if usn == username and pw == password:
+        print("Login berhasil")
+        break
+    else:
+        print("Login gagal")
+        salah += 1
+        
+
+        
+ 
+    
+   
+ 
