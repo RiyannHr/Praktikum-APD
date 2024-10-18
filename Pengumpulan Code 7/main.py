8860b3c5054c2bbae5fff5b46c60cff1efbd3ee2
RiyannHr <riyan.hidayat.rahman1@gmail.com>
2024-10-18 15:09:59 +0800

    Add files via upload

---
+++ Pengumpulan Code 7/main.py
@@ -0,0 +1,92 @@
+# def aku_fungsi ():
+#    print("hello word")
+    
+# def tambah(a, b):
+#    hasil = a+b
+    
+#   print(hasil)
+    
+
+# tambah(10)
+
+# def tambah(a, b)
+# hasil = a+b
+#    return hasil
+    
+#    print = tambah(2, 4)
+#     tambahan = 6 + 10
+    
+#    print(tambahan)
+    
+#    def pilihan():
+#        opssi = input("Masukan pilihan")
+        
+#    def pilihan(opsi):
+#        match opsi:
+#            case opsi:
+#            case    1:
+#                print (1)
+#                return
+#            case 2:
+#                print(2)
+#                return
+#            print(3)
+            
+#    pilihan(2)
+
+# def cetak (nama , nim, matkul)
+#    print(nama,nim,matkul)
+    
+# cetak ("Fathir",41, "Kalkulus","fisika dasar")
+
+var = 2,4,5
+
+var1, var2, var3, = var
+
+# print(var)
+# print(var1)
+# print(var2)
+# print(var3)
+
+Nama = "Informatika"
+Mata_Kuliah = "Algoritma dan Pemrograman Dasar"
+kelas = "IT Asik"
+
+def info():
+    Nama = "Teknik Elektro"
+    Mata_Kuliah = "Pengantar Teknik ELektro"
+    print("prodi:", Nama)
+    print("Mata Kuliah:, Mata_kuliah")
+    print(kelas)
+
+info()
+print("Prodi:, Nama")
+print("Mata Kuliah:, Mata_kuliah")
+
+# dalamdef(10)
+
+def cetak(nama, nim, umur = 18):
+    print(nama,nim,umur)
+    
+if __name__=="__main__":
+    def cetak(nama, nim, umur = 18):
+        print(nama,nim,umur)
+        
+     
+
+
+
+
+
+
+
+
+
+    
+            
+                
+    
+    
+    
+    
+    
